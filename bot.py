4227ac07653aaef7c4bf6f54bf6dd18850e6df2a
mihail-Zav <mihail.zav.13@gmail.com>
2021-06-16 21:24:22 +0300

    Added function for instagram username detection

---
+++ bot.py
@@ -0,0 +1,35 @@
+import telebot
+import time
+
+bot_token = '1869770648:AAHYUzR11CXPmZMRJF21PvnFu06W_lC7MF4'
+
+bot = telebot.TeleBot(token=bot_token)
+
+def find_at(msg):
+    for text in msg:
+        if '@' in text:
+            return text
+
+
+@bot.message_handler(commands=['start'])
+
+def send_welcome(message):
+    bot.reply_to(message, 'Welcome')
+
+@bot.message_handler(commands=['help'])
+
+def send_welcome(message):
+    bot.reply_to(message, 'Send commands starting with "/"')
+
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' is in msg.text)
+def at_answer(message):
+    texts = message.text.split()
+    at_text = find_at(texts)
+    bot.reply_to(message, "https://www.instagram.com/{}".format(at_text[1:]))
+
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
